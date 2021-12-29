e22e0b9224a2524f2eb752801a649c5d99f68d03
Kevin Elliott <skiimilk@gmail.com>
2021-12-30 00:55:47 +0800

    Add visualiser for vite bundle output. Move favicons into public folder.

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,13 @@
 import vue from "@vitejs/plugin-vue";
 import fs from "fs";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   if (mode === "production") {
     return {
-      plugins: [vue()],
+      plugins: [vue(), visualizer({ template: "sunburst" })],
     };
   } else {
     return {
