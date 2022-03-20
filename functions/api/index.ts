4082aed73a8b46fda88c9e89ad39162b995b84eb
Kevin Elliott <skiimilk@gmail.com>
2022-03-20 19:17:13 +0800

    Update

--- functions/api/index.ts
+++ functions/api/index.ts
@@ -1,25 +1,18 @@
-import { Holiday } from "../../types/holiday";
-
-export const onRequest: PagesFunction<{ DATA: KVNamespace }> = async ({
-  request,
-  env,
-}) => {
+export const onRequest: PagesFunction<{
+  DATA: KVNamespace;
+  TEST: KVNamespace;
+}> = async ({ request, env }) => {
   try {
     if (!request.cf)
       throw new Error("No 'cf' properties found on incoming Request.");
 
     const { country, regionCode } = request.cf;
 
-    const holidayData = await env.DATA.get<Holiday[]>(
-      `${country}:${regionCode}`,
-      {
-        type: "json",
-      }
-    );
+    const kv = await env.TEST.get(`akey`, "text");
 
-    if (!holidayData) throw new Error("No holiday data found.");
+    // if (!holidayData) throw new Error("No holiday data found.");
 
-    return new Response(JSON.stringify(holidayData[1]));
+    return new Response(JSON.stringify({ kv }));
   } catch (e) {
     return new Response(
       JSON.stringify({
