8000429996d45d58249f9ec159252c0c3891501a
Kevin Elliott <skiimilk@gmail.com>
2022-03-20 13:03:28 +0800

    Change api function structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ console.log("Start script.js");
 (async function () {
   console.log("Start async func in script.js");
 
-  const response = await fetch("/api");
+  const response = await fetch("/api/data");
   const json = await response.json();
   console.log(json);
 
